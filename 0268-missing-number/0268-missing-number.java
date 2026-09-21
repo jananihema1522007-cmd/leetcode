50ba8125613016a41b30ffceff9a981e275dfabd
JANANI ASOHKAN <jananihema1522007@gmail.com>
2026-09-21 06:13:38 +0530

    Time: 0 ms (100%), Space: 47.6 MB (13.89%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int a = n * (n + 1)/2;
+        int c = 0;
+        for(int b:nums){
+            c = c + b;
+        }
+        return a -c;
+        
+    }
+}
